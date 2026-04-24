9eaae16fb32994a0ede5302db61074aaedeb1444
FocusFlow_vihan <vihandreams@gmail.com>
2026-04-24 15:03:54 +0300

    update sw.js to v2 for better offline caching

--- public/sw.js
+++ public/sw.js
@@ -1,41 +1,46 @@
-const CACHE_NAME = 'focusflow-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/favicon.ico',
-  '/logo192.png',
-  '/logo512.png'
-];
+const CACHE_NAME = 'focusflow-v2'; // Changed to v2 to force an update
 
-// Install the service worker and cache assets
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/favicon.ico',
+        '/logo192.png',
+        '/logo512.png'
+      ]);
     })
   );
 });
 
-// Serve cached content when offline
+// The "Cache First" Strategy
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Return cached version or fetch from network
-      return response || fetch(event.request);
+    caches.match(event.request).then((cachedResponse) => {
+      // Return the cached file if we have it, otherwise go to the network
+      return cachedResponse || fetch(event.request).then((response) => {
+        // If it's a valid response, add it to the cache for next time
+        if (response.status === 200) {
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+        }
+        return response;
+      });
     })
   );
 });
 
-// Clean up old caches
+// Clear old caches
 self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
+          if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
         })
